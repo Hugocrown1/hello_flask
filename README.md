2d613e155182dd80a1e8c4bec0a2429027d60776
Hugocrown1 <hugo.corona.r@gmail.com>
2023-03-12 18:38:30 -0700

    Se consulto una tabla de mysql

--- README.md
+++ README.md
@@ -1,12 +1,22 @@
-# menu
+# Readme chido
 
+
+## Instrucciones
 Aqui van las instrucciones para ejecutar este proyecto
 
-tner instalado python 3.10
+tener instalado python 3.10
 
 a continuacion instalar los archivos requirements.txt
 
 > pip install -r requirements.txt
+
 > pip install mysql-connector
 
 de preferencia esto debe estar en una env
+
+python -m venv  venv
+
+El proyecto esta realizado en flask se debe ejecutar con el siguiente comando:
+
+> flask run app
+> gunicorn
--- hello.py
+++ hello.py
@@ -1,16 +1,51 @@
+import json
 from flask import Flask
+from flask import jsonify
+
+
+import mysql.connector
 app = Flask(__name__)
 
 
-#Activar el virtual env
-#\venv\Scripts\activate.bat
+# Activar el virtual env
+# \venv\Scripts\activate.bat
+
+@app.route('/ligamx/equipos')
+def get_equipos():
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="1234",
+        database="ligamx"
+    )
+
+    mycursor = mydb.cursor()
+
+    mycursor.execute("SELECT * FROM ligamx.equipos")
+
+    results = mycursor.fetchall()
+
+    mydb.close()
+
+    return jsonify(results)
 
 
 @app.route("/")
 def main():
-    return "HOLA ITE"
+    return "HOLA PAMBOLEROS"
 
 
 @app.route("/bye")
 def adios():
-    return "bye bye"
+    return jsonify(despedida="bye bye")
+
+
+@app.route("/about")
+def about():
+    with open("liga.json", "r") as archivo:
+        contenido_json = json.load(archivo)
+        return json.dumps(contenido_json)
+
+
+if __name__ == '__main__':
+    app.run()
